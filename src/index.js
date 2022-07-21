7ae554350b7b699c92a188bc6283bf296a949a68
tnkmatic <tnkmatic@gmail.com>
2022-07-22 07:14:06 +0900

    32. タスクの追加機能(テキスト) 追加ボタンのイベント実装 TODO文字列の取得

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  //テキストボックスの値を取得して初期化
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+  alert(inputText);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
