222e02bbe18cd6edf101082d982307a30d3d08d6
tnkmatic <tnkmatic@gmail.com>
2022-07-23 10:42:41 +0900

    34. タスクの追加機能(ボタン) タスク追加機能を実装 未完了TODOへの追記、完了・削除イベントのリスナーを作成

--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
 import "./styles.css";
 
 const onClickAdd = () => {
-  //テキストボックスの値を取得して初期化
+  //テキストボックスの値を取得して初期化する
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
-  alert(inputText);
+
+  //liタグ生成
+  const li = document.createElement("li");
+  //divタグ生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  //pタグ生成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  //button生成(完了)
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+  //button生成(削除)
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+
+  //DOM構築
+  div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+  li.appendChild(div);
+
+  //未完了リストに追加
+  document.getElementById("imcomplete-list").appendChild(li);
 };
 
 document
