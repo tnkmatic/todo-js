da49e6b43d3426ad7431f862db5354fac88cd57e
tnkmatic <tnkmatic@gmail.com>
2022-07-27 08:40:15 +0900

    37. セクションまとめ リファクタリングを実施中

--- src/index.js
+++ src/index.js
@@ -7,36 +7,10 @@ const onClickAdd = () => {
   // TODOリストのひな形DOMを生成
   const todoRecord = createTodoRecord(inputText);
 
-  //button生成(完了)
-  const completeButton = document.createElement("button");
-  completeButton.innerText = "完了";
-  completeButton.addEventListener("click", () => {
-    // 完了リストに追加するために完了対象を取得
-    const completeTarget = completeButton.parentNode.parentNode;
-    // div(list-row)の要素を取得
-    const divRow = completeTarget.firstChild;
-    // TODOの内容を取得
-    const todoText = divRow.firstElementChild.innerText;
+  
 
-    // 押された完了ボタンの行を未完了リストから消す
-    deleteFromCompleteList(completeTarget);
 
-    // div(list-row)の子要素を初期化
-    divRow.textContent = null;
-    // 完了したTODOに表示するための子要素を生成
-    const p = document.createElement("p");
-    p.innerText = todoText;
-    const backButton = document.createElement("button");
-    backButton.innerText = "戻す";
-    backButton.addEventListener("click", () => {
-      // 完了TODOリストから削除する
-      const backTodo = backButton.parentNode.parentNode;
-      const backTodoText =
-        backTodo.firstElementChild.firstElementChild.innerText;
-      document.getElementById("complete-list").removeChild(backTodo);
-      // 未完了TODOに追加する
-    });
-
+  
     // 要素を組み立てる
     divRow.appendChild(p);
     divRow.appendChild(backButton);
@@ -44,13 +18,6 @@ const onClickAdd = () => {
 
     // 完了したTODOに追加
     document.getElementById("complete-list").appendChild(completeTarget);
-  });
-  //button生成(削除)
-  const deleteButton = document.createElement("button");
-  deleteButton.innerText = "削除";
-  deleteButton.addEventListener("click", () => {
-    // 押された削除ボタンの親タグ(li)を削除する
-    deleteFromCompleteList(deleteButton.parentNode.parentNode);
   });
 
   //DOM構築
@@ -68,11 +35,8 @@ document
 // 未完了TODOリストに追加する
 const insertIncompleteList = (todoText) => {};
 
-// 未完了リストからタスクを削除する
-const deleteFromCompleteList = (deleteTarget) => {
-  document.getElementById("imcomplete-list").removeChild(deleteTarget);
-};
 
+// TODOテキストのDOM要素を生成する
 const createTodoRecord = (todoText) => {
   //liタグ生成
   const li = document.createElement("li");
@@ -87,4 +51,49 @@ const createTodoRecord = (todoText) => {
   li.appendChild(div);
 
   return li;
+};
+
+// 未完了リストにTODO要素を追加する
+const createImcompleteTodo = (todoRecord) => {
+  //button生成(完了)
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    // TODOの内容を取得
+    const todoText = completeButton.parentNode.parentNode.firstElementChild.innerText;
+    // 押された完了ボタンの行を未完了リストから消す
+    deleteFromCompleteList(completeButton.parentNode.parentNode);
+    //完了リストに表示するTODOテキストのDOM要素を生成する
+    const todoRecordForCompleteList = createTodoRecord(todoText);
+  });
+
+  //button生成(削除)
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    // 押された削除ボタンの親タグ(li)を削除する
+    deleteFromCompleteList(deleteButton.parentNode.parentNode);
+  });
+};
+
+// 未完了リストからタスクを削除する
+const deleteFromCompleteList = (deleteTarget) => {
+  document.getElementById("imcomplete-list").removeChild(deleteTarget);
+}
+
+// 完了リストにTODO要素を追加する
+const createCompleteList = (todoRecord) => {
+  // 戻すボタンを生成
+  const backButton = document.createElement("button");
+  backButton.innerText = "戻す";
+  backButton.addEventListener("click", () => {
+    // 完了TODOリストから削除する
+    const backTodo = backButton.parentNode.parentNode;
+    const backTodoText =
+      backTodo.firstElementChild.firstElementChild.innerText;
+    document.getElementById("complete-list").removeChild(backTodo);
+    // 未完了TODOに追加する
+  };
+  //戻すボタンをtodoRecordに追加する
+  todoRecord.firstElementChild.appendChild(backButton); 
 };
